4b3050fb61386c9a13ece4be82b6d52c9875689a
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-24 20:58:12 -0600

    :hammer:Added basic properties to the Character class this will expand as I flesh out the code more.

--- src/main/java/com/example/TheLibrary/models/Character.java
+++ src/main/java/com/example/TheLibrary/models/Character.java
@@ -1,4 +1,19 @@
 package com.example.TheLibrary.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Character {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Guild guild;
+
 }
