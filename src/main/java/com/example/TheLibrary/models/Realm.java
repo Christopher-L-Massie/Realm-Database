58c27673e6afa48427680a2bab44bca7d1942b0d
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-24 21:14:51 -0600

    :hammer:Created basic properties for Realm, I'll fill this out more after thinking about the information that will be needed for it.

--- src/main/java/com/example/TheLibrary/models/Realm.java
+++ src/main/java/com/example/TheLibrary/models/Realm.java
@@ -1,7 +1,20 @@
 package com.example.TheLibrary.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Realm {
+
+    //|||Properties|||
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Faction> factions = new ArrayList<>();
 }
