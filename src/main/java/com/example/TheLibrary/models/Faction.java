67ff33926f54089a7716c519a4d279bfde38b1e2
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-24 21:17:52 -0600

    :hammer:Created properties for Faction class. It will make it easy to display all the guilds or characters on a faction.

--- src/main/java/com/example/TheLibrary/models/Faction.java
+++ src/main/java/com/example/TheLibrary/models/Faction.java
@@ -1,7 +1,23 @@
 package com.example.TheLibrary.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Faction {
+
+    //|||Properties|||
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Character> characters = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Guild> guilds = new ArrayList<>();
 }
