3ebd7e9685677b31c3e44572360f97f7d9a11684
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-24 20:59:07 -0600

    :hammer:Added basic properties to the Guild class. This will get more features as I grow the website more.

--- src/main/java/com/example/TheLibrary/models/Guild.java
+++ src/main/java/com/example/TheLibrary/models/Guild.java
@@ -1,4 +1,20 @@
 package com.example.TheLibrary.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Guild {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private String motto;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Character> members = new ArrayList<>();
 }
