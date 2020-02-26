3c90aa077be97c89c932b50a7ab1fbb43420d09c
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-25 20:17:37 -0600

    :hammer:Added the american and oceanic realm names to the classicRealmNames enum

--- src/main/java/com/example/TheLibrary/models/constants/classicRealmNames.java
+++ src/main/java/com/example/TheLibrary/models/constants/classicRealmNames.java
@@ -2,5 +2,41 @@ package com.example.TheLibrary.models.constants;
 
 public enum classicRealmNames {
 
+    ASHKANDI,
+    ATIESH,
+    AZURESONG,
+    MANKRIK,
+    MYZRAEL,
+    OLD_BLANCHY,
+    PAGLE,
+    WESTFALL,
+    WINDSEEKER,
+    BENEDICITION,
+    BIGGLESWORTH,
+    BLAUMEUX,
+    FAERLINA,
+    FAIRBANKS,
+    HEROD,
+    INCENDIUS,
+    KIRTONOS,
+    KURINAXX,
+    KROMCRUSH,
+    RATTLEGORE,
+    SKERAM,
+    SMOLDERWEB,
+    STALAGG,
+    SULFURAS,
+    THALNOS,
+    THUNDERFURY,
+    WHITEMANE,
+    BLOODSAIL_BUCCANEERS,
+    DEVIATE_DELIGHT,
+    GROBBULUS,
+    ARUGAL,
+    FELSTRIKER,
+    REMULOS,
+    YOJAMBA,
+
+
 
 }
