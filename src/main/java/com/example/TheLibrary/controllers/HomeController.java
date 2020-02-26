34d14cf0649e5c8f55d7187d305be35b6c11e788
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-25 18:56:20 -0600

    :hammer:Created a method that will handle request to view the index of the website.

--- src/main/java/com/example/TheLibrary/controllers/HomeController.java
+++ src/main/java/com/example/TheLibrary/controllers/HomeController.java
@@ -1,9 +1,17 @@
 package com.example.TheLibrary.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "")
 public class HomeController {
+
+    @RequestMapping(value = "")
+    public String displayIndex(Model model){
+        model.addAttribute("title", "Realm Selection");
+
+        return "home/index";
+    }
 }
