8e23770471637f116df5efc1359a25f137033e10
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-03-06 15:59:50 -0600

    :sparkles:Added a handler for when the user request to login to their account.

--- src/main/java/com/example/TheLibrary/controllers/AccountController.java
+++ src/main/java/com/example/TheLibrary/controllers/AccountController.java
@@ -1,9 +1,17 @@
 package com.example.TheLibrary.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("account")
 public class AccountController {
+
+    @RequestMapping(value = "login")
+    public String displayIndex(Model model){
+        model.addAttribute("title", "Login");
+
+        return "account/user-login";
+    }
 }
