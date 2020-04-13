dbc03b0690454b46b86e228eff2e798dd5b528f5
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-04-12 23:38:09 -0500

    :hammer:Setup Gossip.java as a database entity.

--- src/main/java/com/example/TheLibrary/models/Gossip.java
+++ src/main/java/com/example/TheLibrary/models/Gossip.java
@@ -1,4 +1,13 @@
 package com.example.TheLibrary.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Gossip {
+
+    @Id
+    @GeneratedValue
+    private int id;
 }
