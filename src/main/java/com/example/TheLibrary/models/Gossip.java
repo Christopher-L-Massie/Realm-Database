be7017fff2b32c6fac560612f016bf31eaa1bb1d
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-04-13 00:32:23 -0500

    :hammer:Flushed out Gossip class.

--- src/main/java/com/example/TheLibrary/models/Gossip.java
+++ src/main/java/com/example/TheLibrary/models/Gossip.java
@@ -1,13 +1,48 @@
 package com.example.TheLibrary.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.example.TheLibrary.models.Accounts.User;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 public class Gossip {
 
+    //|||Properties|||
+
     @Id
     @GeneratedValue
     private int id;
+
+    private String gossip;
+
+    private Timestamp timeCreated = new Timestamp(System.currentTimeMillis());
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User owner;
+
+    //|||Constructors|||
+    public Gossip(){}
+
+    public Gossip(String gossip, User owner){
+        this.gossip = gossip;
+        this.owner = owner;
+        this.timeCreated = new Timestamp(System.currentTimeMillis());
+    }
+
+    //|||Methods|||
+
+    //|||Accessors|||
+
+    public String getGossip(){
+        return this.gossip;
+    }
+
+    public Timestamp getTimeCreated(){
+        return this.timeCreated;
+    }
+
+    public User getOwner(){
+        return this.owner;
+    }
 }
