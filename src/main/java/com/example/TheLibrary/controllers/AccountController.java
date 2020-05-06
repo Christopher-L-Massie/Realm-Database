2be38b4c62358076b64aec47d8bbedae3f44a6e1
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-05-06 00:09:08 -0500

    :hammer:Changes in AccountController to work with the newly implemented security. Still working out properly calling the .jsp views. I may be missing a dependency.

--- src/main/java/com/example/TheLibrary/controllers/AccountController.java
+++ src/main/java/com/example/TheLibrary/controllers/AccountController.java
@@ -1,25 +1,76 @@
 package com.example.TheLibrary.controllers;
 
+import com.example.TheLibrary.models.Accounts.User;
+import com.example.TheLibrary.service.SecurityService;
+import com.example.TheLibrary.service.UserService;
+import com.example.TheLibrary.service.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("account")
+@RequestMapping("")
 public class AccountController {
 
-    @RequestMapping(value = "login")
-    public String displayIndex(Model model){
-        model.addAttribute("title", "Login");
+    @Autowired
+    private UserService userService;
 
-        return "account/user-login";
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password are invalid!");
+
+        if (logout != null)
+            model.addAttribute("message","You have been logged out successfully.");
+
+        return "account/login";
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model){
+        model.addAttribute("userForm", new User());
+
+        return "account/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult){
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "account/registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getUsername(),userForm.getPasswordConfirm());
+
+        return "redirect:account/welcome";
+    }
+
+    /*
     @RequestMapping(value = "register")
     public String displayRegister(Model model){
         model.addAttribute("title","Register");
 
         return "account/user-register";
+    }
+     */
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
     }
 
     @RequestMapping(value = "setPass")
