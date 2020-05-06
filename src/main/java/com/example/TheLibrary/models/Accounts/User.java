59f2f9c939808d634067db48875305a696145a8d
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-05-06 00:08:11 -0500

    :hammer:Changed 'User.java' to work properly.

--- src/main/java/com/example/TheLibrary/models/Accounts/User.java
+++ src/main/java/com/example/TheLibrary/models/Accounts/User.java
@@ -1,22 +1,28 @@
 package com.example.TheLibrary.models.Accounts;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Set;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     //|||PROPERTIES|||
 
     @Id
-    @GeneratedValue
-    int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    private String userName;
+    private String username;
 
     private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany
+    private Set<Role> roles;
 
     private Timestamp timeCreated = new Timestamp(System.currentTimeMillis());
 
@@ -25,8 +31,8 @@ public class User {
     //empty for database setup
     public User(){}
 
-    public User(String userName, String password){
-        this.userName = userName;
+    public User(String username, String password){
+        this.username = username;
         this.password = password;
     }
 
@@ -34,8 +40,43 @@ public class User {
 
     //|||ACCESSORS|||
 
-    public String getUserName() {
-        return userName;
+    public Long getId() {
+        return id;
     }
-    
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
